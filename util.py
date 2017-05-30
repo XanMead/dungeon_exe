dee056ea065c42b374f7909351a7ca22d5648f20
Xan Mead <x.m.mead@gmail.com>
2017-05-30 16:24:47 -0400

    fixed time formatting

--- util.py
+++ util.py
@@ -3,8 +3,9 @@ import datetime
 def theTime():
     """Returns h:m:s"""
     now = datetime.datetime.now()
-    return '%s:%s:%s' % (now.hour, now.minute, now.second)
+    return '%02d:%02d:%02d' % (now.hour, now.minute, now.second)
 
 def helloTime():
     return "Hello! The time is " + theTime() + "!"
 
+print(theTime())
